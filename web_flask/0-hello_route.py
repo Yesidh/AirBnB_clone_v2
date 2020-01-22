73a36e5baba8caacd56afc4622c244d019a97418
Yesidh <944@holbertonschool.com>
2020-01-21 20:39:02 -0500

    [Fix]: changin run within if statement

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,6 @@
 
 from flask import Flask
 app = Flask(__name__)
-app.run(host='0.0.0.0', port=5000)
 
 
 @app.route('/', strict_slashes=False)
@@ -14,3 +13,6 @@ def index():
     """function that return Hello HBNB"""
 
     return 'Hello HBNB!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
