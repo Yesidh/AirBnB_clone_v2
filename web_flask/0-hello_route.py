8b2c739674c34b4ff09b9e6a038a5325c5c89aa5
Yesidh <944@holbertonschool.com>
2020-01-21 20:18:18 -0500

    [Feature]: script that starts a Flask web application:

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/bin/usr/python3
+# script that starts a Flask web application:
+# Your web application must be listening on 0.0.0.0, port 5000
+# Routes:
+#        /: display “Hello HBNB!”
+
+from flask import Flask
+app = Flask(__name__)
+app.run(host='0.0.0.0')
+
+@app.route('/', strict_slashes=False)
+def _index():
+    return 'Hello HBNB!'
