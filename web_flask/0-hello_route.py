2fe0aa57905c582568ba3789c1c2636395c47d57
Yesidh <944@holbertonschool.com>
2020-01-21 20:27:12 -0500

    [Fix]: adding comments to the function

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,7 +10,10 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def _index():
-    """function that return Hello HBNB"""
+    """
+    function that return Hello HBNB
+    """
+
     return 'Hello HBNB!'
 
 if __name__ = "__main__":
