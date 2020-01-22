2da231707b1edba1fd9f7c2c0676b4123fba4ec6
Yesidh <944@holbertonschool.com>
2020-01-21 20:35:46 -0500

    [Fix]: changing the order for the app.run

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,15 +6,11 @@
 
 from flask import Flask
 app = Flask(__name__)
+app.run(host='0.0.0.0', port=5000)
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    """
-    function that return Hello HBNB
-    """
+    """function that return Hello HBNB"""
 
     return 'Hello HBNB!'
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
