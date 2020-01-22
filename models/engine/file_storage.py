93037e49db7275340dab26d4610693c804d8e143
Yesidh <944@holbertonschool.com>
2020-01-22 13:49:50 -0500

    [Fix]: adding the method close

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,3 +69,8 @@ class FileStorage:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             del self.__objects[key]
             self.save()
+
+    def close(self):
+        """Method for deserializing the JSON file to objects"""
+
+        self.reload()
