1a484da032f5dfb7beadbc66ec1e1c9a895f2189
Yesidh <944@holbertonschool.com>
2020-01-27 00:25:24 -0500

    [Fix]: changing order and comments some lines

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -20,19 +20,12 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_appcontext(self):
-    """Method tho handle teardown"""
-
-    storage.close()
-
-
-@app.route('/states_list', strict_slashes=False)
-def states():
-    """method to show a state list"""
-
-    my_list = storage.all('State').values()
-    return render_template('7-states_list.html', _list=my_list)
+# @app.route('/states_list', strict_slashes=False)
+# def states():
+#    """method to show a state list"""
+#
+#    my_list = storage.all('State').values()
+#    return render_template('7-states_list.html', _list=my_list)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
@@ -41,6 +34,14 @@ def cities_by_states():
 
     my_list = storage.all('State').values()
     return render_template('8-cities_by_states.html', _list=my_list)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(self):
+    """Method tho handle teardown"""
+
+    storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
